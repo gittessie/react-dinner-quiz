c7afdb3479908e050aba4db743349260920d4764
Tessie Urtubia <tessieurtubia@Tessies-MacBook-Pro.local>
2018-04-28 19:14:45 -0400

    add displaying result

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import styles from './Quiz.css';
 import Question from '../../components/Question/Question';
 import QUESTIONS from '../../hoc/QUESTIONS';
+import RESULTS from '../../hoc/RESULTS';
+import Aux from '../../hoc/Aux/Aux';
+import Modal from '../../hoc/Modal/Modal';
 
 class Quiz extends Component {
     state={
@@ -20,7 +23,9 @@ class Quiz extends Component {
             american: 0, 
             mexican: 0, 
             salad: 0            
-        }
+        },
+        showResult: false, 
+        result: null
     }
 
     updateScoresHandler = (scores) => {
@@ -36,16 +41,39 @@ class Quiz extends Component {
           });
     }
 
+    showResultHandler = () => {
+        let currentScores = {...this.state.scores};
+        let finalResult = Object.keys(currentScores).reduce((key1, key2)=> currentScores[key1] > currentScores[key2]? key1 : key2);
+        this.setState({result: finalResult, showResult: true});
+    }
+
+    playAgainHandler = () => {
+        window.location.reload();
+    }
+
     render(){
+        let resultSpace = null; 
+        if(this.state.showResult){
+            resultSpace = (
+                <Modal show={this.state.showResult} modalClose={null}>
+                    <h3>You should go eat {RESULTS[this.state.result].value}</h3>
+                    <p>{RESULTS[this.state.result].text}</p>
+                    <button onClick={this.playAgainHandler}>Reset</button>
+                </Modal>
+            );
+        }
         return(
-            <div className={styles.Quiz}>
-                <h1>What should I eat for dinner?</h1>
-                {QUESTIONS.map((props)=> <Question text={props.text} 
-                                            answers={props.answers} 
-                                            key={props.id}
-                                            scoreUpdateFunc = {this.updateScoresHandler}/>)}
-                <button>Go</button>
-            </div>
+            <Aux>
+                {resultSpace}
+                <div className={styles.Quiz}>
+                    <h1>What should I eat for dinner?</h1>
+                    {QUESTIONS.map((props)=> <Question text={props.text} 
+                                                answers={props.answers} 
+                                                key={props.id}
+                                                scoreUpdateFunc = {this.updateScoresHandler}/>)}
+                    <button onClick={this.showResultHandler}>Go</button>
+                </div>
+            </Aux>
         );
     }
 }
---
+++ src/hoc/RESULTS.js
@@ -0,0 +1,18 @@
+const RESULTS = {
+    pizza: {value: 'Pizza', text: 'You can never go wrong with pizza.'},
+    vegan_pizza: {value: 'Vegan Pizza', text: 'You can never go wrong with pizza.'},
+    italian: {value: 'Italian Food', text: 'Time to indulge on allllll the carbs!'}, 
+    indian: {value: 'Indian Food', text: 'Can you handle all that spice?'},
+    tapas: {value: 'Tapas', text: 'So much to choose from! Perfect for the indecisive.'}, 
+    middle_eastern: {value: 'Middle Eastern Food', text: "Yummy falafel or schwarma? Yes, please."},
+    bbq: {value: 'BBQ', text: 'Time for a quick trip to Texas.'}, 
+    chinese: {value: 'Chinese Food', text: 'An oldie but a goodie.'}, 
+    thai: {value: 'Thai Food', text: 'I hope you like spicy food!'}, 
+    ramen: {value: 'Ramen', text: "It's good for the soul."}, 
+    breakfast: {value: 'Breakfast Foods', text: 'Time to head to the nearest diner!'}, 
+    american: {value: 'American Food', text: 'Go get yourself some burgers and onion rings!'}, 
+    mexican: {value: 'Mexican Food', text: '...and  maybe order some margaritas too.'}, 
+    salad: {value: 'a Salad', text: "Looks like you're eating light and healthy tonight!"}            
+};
+
+export default RESULTS;
